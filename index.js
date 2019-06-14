1ae805c3f3c0ab5579f234ce0055024c20fbc984
Eric Storey <estorey@gmail.com>
2019-06-14 21:19:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function distanceFromHqInFeet(block){
   return distanceFromHqInBlocks(block)*264
 }
 
-function distanceFromHqInFeet(block){
-  return distanceFromHqInBlocks(block)*264
+function distanceTravelledInFeet(block1, block2){
+  return Math.abs((block1-block2)*264)
 }
