888f1b9376871d9f845e257bd9f44c417cc627f2
Eric Storey <estorey@gmail.com>
2019-06-14 21:28:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,13 +13,8 @@ function distanceTravelledInFeet(block1, block2){
 
 function calculatesFarePrice(block1, block2){
   const feet=distanceTravelledInFeet(block1, block2)
-  if feet<400{
-    return 0;
-  }else if feet<2000{
-    return (feet-400)*.02
-  }else if feet<2500{
-    return 25
-  }else {
-    return 'cannot travel that far'
-  }
+  if (feet<400){return 0;}
+  else if (feet<2000){return (feet-400)*.02}
+  else if (feet<2500){return 25}
+  else {return 'cannot travel that far'}
 }
